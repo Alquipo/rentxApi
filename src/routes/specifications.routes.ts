7ab5967f8e34168b5d04a56846959018af762e35
Alquipo <alquiponeto@outlook.com.br>
2021-04-04 18:25:42 -0300

    fix: specification implementations

--- src/routes/specifications.routes.ts
+++ src/routes/specifications.routes.ts
@@ -1,28 +1,11 @@
 import { Router } from "express";
 
-import { SpecificationsRepository } from "../modules/cars/repositories/implementations/SpecificationsRepository";
-import { CreateSpecificationService } from "../modules/cars/services/CreateSpecificationService";
+import { createSpecificationController } from "../modules/cars/useCases/createSpecification";
 
 const specificationRoutes = Router();
 
-const specificationsRepository = new SpecificationsRepository();
-
 specificationRoutes.post("/", (request, response) => {
-  const { name, description } = request.body;
-
-  const createSpecificationService = new CreateSpecificationService(
-    specificationsRepository
-  );
-
-  createSpecificationService.execute({ name, description });
-
-  return response.status(201).send();
+  return createSpecificationController.handle(request, response);
 });
-
-// specificationRoutes.get("/", (request, response) => {
-//   const all = categoriesRepository.list();
-
-//   return response.json(all);
-// });
 
 export { specificationRoutes };
