b4bf220d485f02619be7d9a90492312d6b2649e7
Alquipo <alquiponeto@outlook.com.br>
2021-04-06 16:19:25 -0300

    fix: userDuplication

--- src/modules/accounts/useCases/createUser/CreateUserUseCase.ts
+++ src/modules/accounts/useCases/createUser/CreateUserUseCase.ts
@@ -17,6 +17,11 @@ class CreateUserUseCase {
     email,
     password,
   }: ICreateUserDTO): Promise<void> {
+    const userAlreadyExists = await this.userRepository.findByEmail(email);
+
+    if (userAlreadyExists) {
+      throw new Error("User already exists!");
+    }
     const passwordHash = await hash(password, 8);
 
     await this.userRepository.create({
