4524235eab94aaf1494954f4f9000c844c6ba20b
Alquipo <alquiponeto@outlook.com.br>
2021-04-06 16:16:09 -0300

    fix: criptografia da senha

--- src/modules/accounts/useCases/createUser/CreateUserUseCase.ts
+++ src/modules/accounts/useCases/createUser/CreateUserUseCase.ts
@@ -1,3 +1,4 @@
+import { hash } from "bcrypt";
 import { inject, injectable } from "tsyringe";
 
 import { ICreateUserDTO } from "../../dtos/ICreateUserDTO";
@@ -16,11 +17,13 @@ class CreateUserUseCase {
     email,
     password,
   }: ICreateUserDTO): Promise<void> {
+    const passwordHash = await hash(password, 8);
+
     await this.userRepository.create({
       name,
       driver_license,
       email,
-      password,
+      password: passwordHash,
     });
   }
 }
