8be1b22371b34af39a93b724984fd405ff3e69b6
Alquipo <alquiponeto@outlook.com.br>
2021-06-02 18:19:47 -0300

    fix: refactoring infra folder

--- src/shared/infra/http/routes/specifications.routes.ts
+++ src/shared/infra/http/routes/specifications.routes.ts
@@ -1,7 +1,8 @@
 import { Router } from "express";
-import { ensureAuthenticated } from "middlewares/ensureAuthenticated";
 
 import { CreateSpecificationController } from "@modules/cars/useCases/createSpecification/CreateSpecificationsController";
+
+import { ensureAuthenticated } from "../middlewares/ensureAuthenticated";
 
 const specificationRoutes = Router();
 
