b4c9701a6890c8c317edfd6f69b7c1066a2b82d4
Alquipo <alquiponeto@outlook.com.br>
2021-06-02 17:56:28 -0300

    fix: all imports fixed

--- src/routes/specifications.routes.ts
+++ src/routes/specifications.routes.ts
@@ -1,7 +1,7 @@
 import { Router } from "express";
+import { ensureAuthenticated } from "middlewares/ensureAuthenticated";
 
-import { ensureAuthenticated } from "../middlewares/ensureAuthenticated";
-import { CreateSpecificationController } from "../modules/cars/useCases/createSpecification/CreateSpecificationsController";
+import { CreateSpecificationController } from "@modules/cars/useCases/createSpecification/CreateSpecificationsController";
 
 const specificationRoutes = Router();
 
